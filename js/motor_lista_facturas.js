c7c1f0d6176ec902b439c4aa629ac615584e8f2d
1RD4 <adrian.florez.santalla@gmail.com>
2022-02-09 13:41:23 +0100

    Lista js, Detalle html/js

--- js/motor_lista_facturas.js
+++ js/motor_lista_facturas.js
@@ -1,3 +1,5 @@
+//LISTA DOCUMENTOS NODOO
+
 let nav_facturas = document.createElement("div");
 nav_facturas.setAttribute("id", "nav_facturas");
 
@@ -35,6 +37,9 @@ cuerpo_facturas.appendChild(nav_facturas);
 let lista_facturas = document.createElement("div");
 lista_facturas.setAttribute("id", "lista_facturas");
 
+
+//CREACION TEMPORAL DE ELEMENTOS DE LA LISTA
+
 for (let i = 0; i < 5; i++) {
 
     let div_factura_blanco = document.createElement("div");
@@ -48,17 +53,17 @@ for (let i = 0; i < 5; i++) {
     lista_facturas.appendChild(div_factura_rojo)
 
     let div_campo_factura_id = document.createElement("div");
-    div_campo_factura_id.setAttribute("class", "campos_facturas");
+    div_campo_factura_id.setAttribute("class", "parametro_factura");
     let div_campo_factura_empresa = document.createElement("div");
-    div_campo_factura_empresa.setAttribute("class", "campos_facturas");
+    div_campo_factura_empresa.setAttribute("class", "parametro_factura");
     let div_campo_factura_fecha = document.createElement("div");
-    div_campo_factura_fecha.setAttribute("class", "campos_facturas");
+    div_campo_factura_fecha.setAttribute("class", "parametro_factura");
     let div_campo_factura_pago = document.createElement("div");
-    div_campo_factura_pago.setAttribute("class", "campos_facturas");
+    div_campo_factura_pago.setAttribute("class", "parametro_factura");
     let div_campo_factura_cantidad = document.createElement("div");
-    div_campo_factura_cantidad.setAttribute("class", "campos_facturas");
+    div_campo_factura_cantidad.setAttribute("class", "parametro_factura");
     let div_campo_factura_importe = document.createElement("div");
-    div_campo_factura_importe.setAttribute("class", "campos_facturas");
+    div_campo_factura_importe.setAttribute("class", "parametro_factura");
 
     div_factura_blanco.appendChild(div_campo_factura_id);
     div_factura_blanco.appendChild(div_campo_factura_empresa);
