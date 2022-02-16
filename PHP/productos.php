80d0b623926240ef60174e68c2d6e8f1dd6ecf69
TheLordRalph <raulete10m@gmail.com>
2022-02-16 12:13:35 +0100

    Terminado la pagina de producto relacionada con la base de Datos. Falta terminar Pop-up de productos.

--- PHP/productos.php
+++ PHP/productos.php
@@ -1,8 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "10.0.7.47";
+$username = "Admin";
+$password = "Admin1234";
 $database = "nodoo";
+
+$respuesta=array();
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $database);
@@ -11,11 +13,14 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$getdestinos = "select * from empresa where 1";
+$getdestinos = "select * from producto";
 $result = mysqli_query($conn, $getdestinos)
 	or die ("Insert Error");
 
 while($row = mysqli_fetch_array($result)) {
-	echo $row['nombre'];
+	array_push($respuesta,[$row[0],$row[2],$row[1],$row[5],$row[6],$row[4],$row[7]]);
 }
+
+$myJSON = json_encode($respuesta);
+echo $myJSON;
 ?>
---
+++ js/productos.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+    lista_productos();
+}
+
+
+function lista_productos() {
+    let columnas_producto = ["Id Producto", "Nombre Producto", "Categoria", "Precio Compra", "Precio Venta", "Stock", "IVA"];
+
+    let lista_productos = document.getElementById("lista_productos");
+
+    columnas_producto.forEach(element => {
+        let columna_producto = document.createElement("div");
+        let nombre_columna_producto = document.createElement("h4");
+
+        nombre_columna_producto.innerHTML = element;
+        columna_producto.appendChild(nombre_columna_producto);
+        lista_productos.appendChild(columna_producto);
+    });
+
+    let productos;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let tabla_productos = document.getElementById("tabla_productos");
+            let fila_producto
+
+            productos = JSON.parse(this.responseText);
+
+            productos.forEach(i => {
+                fila_producto = document.createElement("div");
+
+                for (let j = 0; j < i.length; j++) {
+                    let valor_fila_producto = document.createElement("h4");
+                    valor_fila_producto.innerHTML = i[j];
+                    fila_producto.appendChild(valor_fila_producto);
+                }
+                tabla_productos.appendChild(fila_producto);
+            });
+        }
+    };
+    xhttp.open("GET", "./PHP/productos.php", true);
+    xhttp.send();
+}
